15fbef3e33474eed80d0dd0783effc12e17d80bc
Osamu Urakawa <ourakawa@gmail.com>
2025-12-11 07:48:40 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react()],
+    build: {
+      chunkSizeWarningLimit: 1600,
+    },
     define: {
       'process.env.API_KEY': JSON.stringify(env.API_KEY)
     }
